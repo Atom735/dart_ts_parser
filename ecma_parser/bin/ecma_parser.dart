fd353bcf8e0f58a0a929db829fd92772699c9428
Atom735 <Atom735.ag47@gmaIl.com>
2021-08-01 01:42:29 +0300

    init commit

---
+++ ecma_parser/bin/ecma_parser.dart
@@ -0,0 +1,5 @@
+import 'package:ecma_parser/ecma_parser.dart' as ecma_parser;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ecma_parser.calculate()}!');
+}
---
+++ ecma_parser/test/ecma_parser_test.dart
@@ -0,0 +1,8 @@
+import 'package:ecma_parser/ecma_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
